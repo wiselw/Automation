0aee21455fabafa61a372d181f6019f8c8227ac5
wiselw <wiselw@gmail.com>
2014-04-21 17:51:16 +0800

    addtestng demo

---
+++ AutomationInTestNGTest/src/com/automation/test/TestNGTest.java
@@ -0,0 +1,62 @@
+package com.automation.test;
+
+import org.testng.AssertJUnit;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class TestNGTest {
+  @Test(dataProvider = "dp")
+  public void DataDrivenTestDemo(Integer n, String s) {
+	  //AssertJUnit.assertEquals(expected, actual)
+	  Reporter.log(s);
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a"},
+      new Object[] { 2, "b" },
+    };
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
